ed69d18e96f581c24569133e78eaced08fe295b3
Rene <rrr333nnn333@gmail.com>
2026-07-03 14:44:22 -0400

    drop: "/data/media/0" paths hiding

--- boot-completed.sh
+++ boot-completed.sh
@@ -131,20 +131,6 @@ if [[ "${config_paths_hiding__non_standard_sdcard}" == "1" ]]; then
 
 		brene_sus_path_loop "${i}"
 	done
-
-	for i in /data/media/0/*; do
-		pass=0
-		for x in ${standard_paths}; do
-			if [[ "/data/media/0/${x}" == "${i}" ]]; then
-				pass=1
-				break
-			fi
-		done
-
-		[[ "${pass}" == "1" ]] && continue
-
-		brene_sus_path_loop "${i}"
-	done
 fi
 
 # Non-standard /sdcard/Android
@@ -209,40 +195,24 @@ if [[ "${config_paths_hiding__sdcard_android_data_media_obb}" == "1" ]]; then
 
 	for i in ${packages}; do
 		path1=/sdcard/Android
-		path2=/data/media/0/Android
 		full_path1="${path1}/data/${i}"
 		full_path2="${path1}/media/${i}"
 		full_path3="${path1}/obb/${i}"
-		full_path4="${path2}/data/${i}"
-		full_path5="${path2}/media/${i}"
-		full_path6="${path2}/obb/${i}"
 		[[ -e "${full_path1}" ]] && brene_sus_path_loop "${full_path1}"
 		[[ -e "${full_path2}" ]] && brene_sus_path_loop "${full_path2}"
 		[[ -e "${full_path3}" ]] && brene_sus_path_loop "${full_path3}"
-		[[ -e "${full_path4}" ]] && brene_sus_path_loop "${full_path4}"
-		[[ -e "${full_path5}" ]] && brene_sus_path_loop "${full_path5}"
-		[[ -e "${full_path6}" ]] && brene_sus_path_loop "${full_path6}"
 	done
 
 	# path1=/sdcard/Android/data
 	# path2=/sdcard/Android/media
 	# path3=/sdcard/Android/obb
-	# path4=/data/media/0/Android/data
-	# path5=/data/media/0/Android/media
-	# path6=/data/media/0/Android/obb
 	# for i in $(pm list packages -3 | cut -d':' -f2); do
 	# 	full_path1="${path1}/${i}"
 	# 	full_path2="${path2}/${i}"
 	# 	full_path3="${path3}/${i}"
-	# 	full_path4="${path4}/${i}"
-	# 	full_path5="${path5}/${i}"
-	# 	full_path6="${path6}/${i}"
 	# 	[[ -e "${full_path1}" ]] && brene_sus_path_loop "${full_path1}"
 	# 	[[ -e "${full_path2}" ]] && brene_sus_path_loop "${full_path2}"
 	# 	[[ -e "${full_path3}" ]] && brene_sus_path_loop "${full_path3}"
-	# 	[[ -e "${full_path4}" ]] && brene_sus_path_loop "${full_path4}"
-	# 	[[ -e "${full_path5}" ]] && brene_sus_path_loop "${full_path5}"
-	# 	[[ -e "${full_path6}" ]] && brene_sus_path_loop "${full_path6}"
 	# done
 fi
 
